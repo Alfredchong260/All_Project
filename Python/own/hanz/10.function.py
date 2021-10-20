8a82cf5ea7be677dcbba60ee3c328ec123899d24
Alfredchong260 <alfredchong260@gmail.com>
2021-10-20 11:46:52 +0800

    minor change

--- Python/own/hanz/10.function.py
+++ Python/own/hanz/10.function.py
@@ -1,5 +1,12 @@
 # A function is a block of code which only runs when it is called
 
+
+'''
+    1.无参数，无返回值
+    2.无参数，有返回值
+    3.有参数，无返回值
+    4.有参数，有返回值
+'''
 # Create function
 def saysomthing(str):
     return (f"Hello {str}")
@@ -10,6 +17,44 @@ def Sum(num1, num2):
     return total
 
 
+total = Sum(1, 2)
+# print(total)
+# print(Sum(2,2))
+
+# tuple
+# Arguments
+def sumofnumber(*args):
+    num = 0
+    for arg in args:
+        num += arg
+    return num
+
+# total = sumofnumber(1,2,3,4,5,6,7,8,9,0)
+# print(total)
+
+# kwargs
+# Keyword arguments
+# 关键字参数
+
+def something(k='Hello'):
+    print(k)
+
+# something(k='How are you')
+
+def keywordargument(**kwargs):
+    print(kwargs)
+
+keywordargument(k='Hello', w='Word')
+
+
+
+
+
+
+
+
+
+
 # Another usage of function
-getSum = lambda num1, num2: num1 + num2
+# getSum = lambda num1, num2: num1 + num2
 
--- Python/own/hanz/target/login.py
+++ Python/own/hanz/target/login.py
@@ -3,3 +3,78 @@ login system
     思路：
         增删改查
 '''
+
+# Dictionary not allow duplicate, overwrite
+# information = {"Username":"Alfred", 'Password': '123456789'}
+# print(information['Username'])
+# print(infos[4])
+
+infos = [
+    {'Username':'CheHin','Password':'123456'},
+    {'Username':'John','Password':'John123'},
+    {'Username':'Steven','Password':'Steven123'},
+]
+
+welcome = '''Welcome to the First Version of the Login System
+    1. Login
+    2. Delete
+    3. Modify
+    4. Search
+    5. Exit
+'''
+
+
+flag = 1
+while flag:
+    print(welcome)
+    choice = input('Please enter your choice:')
+    if choice == '1':
+        username = input('Please enter your username:')
+        password = input('Please enter your password:')
+        for info in infos:
+            if info['Username'] == username:
+                if info['Password'] == password:
+                    print('Login')
+                    flag = 0
+
+                else:
+                    print('Password Incorrect')
+
+    elif choice == '2':
+        username = input('Please enter your username:')
+        password = input('Please enter your password:')
+        for info in infos:
+            if info['Username'] == username:
+                print('Account found')
+                double_confirm = input('Are you sure want to delete account ? Y/N :')
+                if double_confirm.upper() == 'Y':
+                    infos.remove(info)
+                elif double_confirm.upper() == 'N':
+                    continue
+                else:
+                    print('Invalid Input')
+
+    elif choice == '3':
+        username = input('Please enter your username:')
+        password = input('Please enter your password:')
+        for info in infos:
+            if info['Username'] == username:
+                if info['Password'] == password:
+                    new_password = input('Please enter your new password:')
+                    info['Password'] = new_password
+                    print('Password changed')
+                else:
+                    print('Incorrect password')
+
+    elif choice == '4':
+        username = input('Please enter your username:')
+        password = input('Please enter your password:')
+        for info in infos:
+            if info['Username'] == username:
+                if info['Password'] == password:
+                    print(f'Username:{username}\nPassword:{password}')
+                else:
+                    print('Incorrect password')
+
+    if choice == '5':
+        break
