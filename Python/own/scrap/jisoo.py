725022550f87fd3b0dabd2a481ea44aa7fbdf102
Alfred726 <alfredchong260@gmail.com>
2022-10-20 01:18:19 +0800

    commit

--- Python/own/scrap/jisoo.py
+++ Python/own/scrap/jisoo.py
@@ -8,4 +8,4 @@ links = ['https://www.wumaow.org/tu/2887464_5.html', 'https://www.wumaow.org/tu/
 for link in links:
     response = requests.get(link)
     selector = parsel.Selector(response.text)
-    break
+    selecter.css()
